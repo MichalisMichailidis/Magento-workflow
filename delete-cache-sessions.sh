66b5470f21366e3290e82c7387a8bc2157649909
Pericles Bogris <Periklisbogris@gmail.com>
2017-09-28 12:27:55 +0300

    Add files via upload

---
+++ delete-cache-sessions.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ 
+rm -rf path-of-the-site-directory/public_html/var/cache/*
+rm -rf path-of-the-site-directory/public_html/var/session/*
+rm -rf path-of-the-site-directory/public_html/var/report/*
